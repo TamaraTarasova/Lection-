cc9b811950376bbd9f59b5829f714cd94754bd22
Tamara <2010nfvfhf2010@gmail.com>
2023-02-25 23:30:26 +0400

    ToLower

---
+++ Primer5_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("введите имя пользователя");
+string username = Console.ReadLine();
+if (username.ToLower() == " маша")
+{
+  Console.WriteLine("Ура это же Маша!");
+}
+else
+{
+  Console.Write("Привет,");
+  Console.WriteLine(username);
+}
