ec716f7e5bcbc5818edc45dd5eb31898790567bf
Tamara <2010nfvfhf2010@gmail.com>
2023-02-26 23:02:05 +0400

    find index

---
+++ Primer10/Program.cs
@@ -0,0 +1,15 @@
+﻿// поиск позиции нужного элемента
+int[] array = {1, 52, 3, 8, 13, 5, 3};
+int n = array.Length;
+int find = 3;
+int index = 0;
+while (index<n)
+{
+  if (array[index]==find)
+  {
+    Console.WriteLine(index);
+    break;
+  }
+
+  index++;
+}
